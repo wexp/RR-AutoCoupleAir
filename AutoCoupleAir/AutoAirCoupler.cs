ea7404608357c9da4c95c44c5f379f48e0ac6295
wexp <wexpmail@gmail.com>
2024-03-24 14:15:31 +0100

    Add project files.

---
+++ AutoCoupleAir/AutoAirCoupler.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using Railloader;
+using UI.Builder;
+
+namespace AutoCoupleAir
+{
+    internal class Settings
+    {
+        public bool isEnabled = true;
+        public bool affectHandbrakes = true;
+        public bool affectOnlyAi = false;
+        public Settings()
+        {
+        }
+    }
+
+    public class AutoAirCoupler : PluginBase, IModTabHandler
+    {
+        private readonly IModDefinition self;
+        private readonly IModdingContext moddingContext;
+        internal static Settings Settings;
+        public AutoAirCoupler(IModDefinition self, IModdingContext moddingContext)
+        {
+            AutoAirCoupler.Settings = moddingContext.LoadSettingsData<Settings>(self.Id) ?? new Settings();
+            this.self = self;
+            this.moddingContext = moddingContext;
+            new Harmony("wexp.AutoCoupleAir").PatchAll(GetType().Assembly);
+        }
+
+        public void ModTabDidClose()
+        {
+            this.moddingContext.SaveSettingsData<Settings>(this.self.Id, AutoAirCoupler.Settings);
+        }
+
+        public void ModTabDidOpen(UIPanelBuilder builder)
+        {
+            builder.AddLabel("Auto attach hoses & open anglecocks on couple");
+            builder.AddField("Enabled ", builder.AddToggle(() => AutoAirCoupler.Settings.isEnabled, delegate (bool q)
+                {
+                    AutoAirCoupler.Settings.isEnabled = q;
+                }));
+            builder.AddField("Affect only yard AI ", builder.AddToggle(() => AutoAirCoupler.Settings.affectOnlyAi, delegate (bool q)
+            {
+                AutoAirCoupler.Settings.affectOnlyAi = q;
+            }));
+            builder.AddField("Turn off handbrakes:", builder.AddToggle(() => AutoAirCoupler.Settings.affectHandbrakes, delegate (bool q)
+            {
+                AutoAirCoupler.Settings.affectHandbrakes = q;
+            }));
+            
+        }
+    }
+}
---
+++ AutoCoupleAir/AutoCoupleAir.cs
@@ -0,0 +1,66 @@
+﻿using Game;
+using Game.Messages;
+using Game.State;
+using HarmonyLib;
+using Model;
+using Model.AI;
+using RollingStock;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AutoCoupleAir
+{
+
+    [HarmonyPatch(typeof(Car), "HandleCoupledChange")]
+    public class AutoCoupleAir
+    {
+        static void Postfix(Car __instance)
+        {
+            AutoEngineerPersistence persistence = new AutoEngineerPersistence(__instance.KeyValueObject);
+            bool flag = false;
+            if (AutoAirCoupler.Settings.affectOnlyAi)
+            {
+                if (persistence.Orders.Enabled)
+                {
+                    flag = true;
+                }
+                else { flag = false; }
+            }
+            else { flag = true; }
+            if (AutoAirCoupler.Settings.isEnabled && flag && StateManager.CheckAuthorizedToChangeProperty(StateManager.Shared.PlayersManager.LocalPlayer.PlayerId.String, Car.EndGearStateKey.Anglecock.ToString()) && (StateManager.IsHost || TrainController.Shared.SelectedCar.id == __instance.id))
+            {
+                IEnumerable<Car> coupledCars = __instance.EnumerateCoupled();
+
+                foreach (var car in coupledCars)
+                {
+                    if (car.EndGearA.IsCoupled)
+                    {
+                        car.EndGearA.Anglecock.GladhandClick();
+                        var othercar = car.CoupledTo(Car.LogicalEnd.A);
+                        StateManager.ApplyLocal(new SetGladhandsConnected(car.id, othercar.id, true));
+                        if (car.EndGearA.IsAirConnected)
+                        {
+                            car.ApplyEndGearChange(Car.LogicalEnd.A, Car.EndGearStateKey.Anglecock, 1f);
+                        }
+                    }
+                    if (car.EndGearB.IsCoupled)
+                    {
+                        car.EndGearB.Anglecock.GladhandClick();
+                        var othercar = car.CoupledTo(Car.LogicalEnd.B);
+                        StateManager.ApplyLocal(new SetGladhandsConnected(car.id, othercar.id, true));
+                        if (car.EndGearA.IsAirConnected)
+                        {
+                            car.ApplyEndGearChange(Car.LogicalEnd.B, Car.EndGearStateKey.Anglecock, 1f);
+                        }
+                    }
+                    if (AutoAirCoupler.Settings.affectHandbrakes)
+                    {
+                        StateManager.ApplyLocal(new PropertyChange(car.id, PropertyChange.Control.Handbrake, false));
+                    }
+                    else { continue; }
+                }
+            }
+        }
+    }
+}
